f328b8fc4c2a6a3c6231944856f0e81e5c94b7a6
MFALLAM <mohammed.fathi.allam@gmail.com>
2023-12-20 10:45:29 +0300

    created data converter

--- app/src/main/java/com/example/android/todolist/database/TaskEntry.java
+++ app/src/main/java/com/example/android/todolist/database/TaskEntry.java
@@ -1,5 +1,6 @@
 package com.example.android.todolist.database;
 
+import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.Ignore;
 import android.arch.persistence.room.PrimaryKey;
